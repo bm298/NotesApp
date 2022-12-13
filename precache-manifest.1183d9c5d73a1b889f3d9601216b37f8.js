f520885b29890d680171a687c5532390f6603521
bm298 <musabilal93@gmail.com>
2022-12-13 23:53:22 +0000

    Updates

---
+++ precache-manifest.1183d9c5d73a1b889f3d9601216b37f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d7557d8803220fe29e2",
+    "url": "/NotesApp/static/css/main.ac7656f1.chunk.css"
+  },
+  {
+    "revision": "4d7557d8803220fe29e2",
+    "url": "/NotesApp/static/js/main.4d7557d8.chunk.js"
+  },
+  {
+    "revision": "271d1efb744527e0afa6",
+    "url": "/NotesApp/static/js/1.271d1efb.chunk.js"
+  },
+  {
+    "revision": "f41e53477a2e41ba4fd8",
+    "url": "/NotesApp/static/js/2.f41e5347.chunk.js"
+  },
+  {
+    "revision": "3c9691db40a852997b45",
+    "url": "/NotesApp/static/js/runtime~main.3c9691db.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/NotesApp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "68301bffe32eb9396829163c95f27701",
+    "url": "/NotesApp/index.html"
+  }
+];
